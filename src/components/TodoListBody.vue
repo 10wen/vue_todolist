3bb6cab30ca38418aa7f95b319e6b9b4d8d39305
10wen <844834411@qq.com>
2022-09-26 13:42:54 +0800

    添加进入离开动画\

--- src/components/TodoListBody.vue
+++ src/components/TodoListBody.vue
@@ -1,6 +1,8 @@
 <template>
   <ul class="todo-main">
-    <TodoListItem v-for="todo of todos" :key="todo.id" :todo="todo"></TodoListItem>
+    <transition-group name="todo" appear>
+      <TodoListItem v-for="todo of todos" :key="todo.id" :todo="todo"></TodoListItem>
+    </transition-group>
   </ul>
 </template>
 
@@ -31,4 +33,21 @@ export default {
   padding-left: 5px;
   margin-top: 10px;
 }
+
+@keyframes todoItemAnimate {
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0px);
+  }
+}
+
+.todo-enter-active {
+  animation: todoItemAnimate 0.25s linear;
+}
+.todo-leave-active {
+  animation: todoItemAnimate 0.25s linear reverse;
+}
+
 </style>
